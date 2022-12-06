7a55864aa3191d68b7f145991077317931d10c2f
Fra-Battaglia <francescobattaglia03@gmail.com>
2022-12-06 21:41:31 +0100

    exercise done

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function generate_cell(number) {
+	let cell = document.createElement('div');
+	cell.classList.add('grid-cell')
+	cell.innerText = number;
+	return cell
+}
+
+// 1 - creo un bottone che se cliccato genera una griglia con all'interno delle celle numerate
+let generate_grid = document.getElementById('grid-generator');
+generate_grid.addEventListener('click', function() {
+	for (let i = 1; i <= 100; i++) {
+		let grid = document.getElementById('container-grid');
+		let grid_cell = generate_cell(i);
+		grid.append(grid_cell);
+
+		grid_cell.addEventListener('click', function(){
+			console.log(this.innerText);
+			this.classList.add('clicked')
+		})
+	}
+});
